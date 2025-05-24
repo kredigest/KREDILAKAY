62669617ab1c58d5f4f8d8739ccb872bb95c6350
kredigest <manfouben12@gmail.com>
2025-05-24 14:33:30 -0400

    Update errors.py

--- app/routes/errors.py
+++ app/routes/errors.py
@@ -1,1 +1,18 @@
-{\rtf1}
+from flask import jsonify
+from werkzeug.exceptions import HTTPException
+
+def register_error_handlers(app):
+    @app.errorhandler(HTTPException)
+    def handle_http_error(e):
+        return jsonify({
+            'code': e.code,
+            'message': e.description
+        }), e.code
+
+    @app.errorhandler(Exception)
+    def handle_unexpected_error(e):
+        app.logger.error(f'Unexpected error: {str(e)}')
+        return jsonify({
+            'code': 500,
+            'message': 'Erreur interne du serveur'
+        }), 500
