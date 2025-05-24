8d10d3438bbf8ac9e82a110e97fd6419903d66b8
kredigest <manfouben12@gmail.com>
2025-05-24 14:36:28 -0400

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,13 @@
 from flask import Flask
+from .routes import init_app as init_routes
 
 def create_app():
     app = Flask(__name__)
-    app.config['SECRET_KEY'] = 'your-secret'
+    
+    # Configuration
+    app.config.from_pyfile('config.py')
+    
+    # Initialisation des routes
+    init_routes(app)
+    
     return app
