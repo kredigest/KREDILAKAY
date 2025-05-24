be29ecb657766024e6edb0814c79cf9fd4c80770
kredigest <manfouben12@gmail.com>
2025-05-24 15:00:05 -0400

    Update auth.py

--- shemas/auth.py
+++ shemas/auth.py
@@ -1,1 +1,13 @@
-{\rtf1}
+from pydantic import BaseModel, EmailStr, constr
+
+class AuthSchema(BaseModel):
+    email: EmailStr
+    password: constr(min_length=8, max_length=64)
+    
+    class Config:
+        json_schema_extra = {
+            "example": {
+                "email": "client@kredilakay.ht",
+                "password": "Str0ngP@ss"
+            }
+        }
