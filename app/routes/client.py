aee50f25404962851c7e74701f75abd657084e9b
kredigest <manfouben12@gmail.com>
2025-05-24 14:34:21 -0400

    Update client.py

--- app/routes/client.py
+++ app/routes/client.py
@@ -1,1 +1,11 @@
-{\rtf1}
+from flask_restx import Namespace, Resource
+from app.services.auth import roles_required
+
+api = Namespace('client', description='Opérations clients')
+
+@api.route('/loans')
+class ClientLoans(Resource):
+    @roles_required('client')
+    def get(self):
+        """Liste des prêts du client"""
+        return {"loans": []}, 200
