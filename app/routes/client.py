eb7bb8dcda43c30bbffa93794076162751603dec
kredigest <manfouben12@gmail.com>
2025-05-25 20:34:40 -0400

    Update client.py

--- app/routes/client.py
+++ app/routes/client.py
@@ -1,13 +1,10 @@
-# app/routes/client.py
 from flask_restx import Namespace, Resource
-from app.services.auth import roles_required
-from flask_jwt_extended import jwt_required
+from app.services.auth import AuthService
 
-api = Namespace('client', description='Espace client sécurisé')
+api = Namespace('client', description='Espace sécurisé client')
 
-@api.route('/dashboard')
-class ClientDashboard(Resource):
-    @jwt_required()
-    @roles_required('client', 'premium_client')
+@api.route('/espace')
+class ClientEspace(Resource):
+    @AuthService.require_roles('client', 'premium')
     def get(self):
-        return {'message': 'Bienvenue, client !'}
+        return {"message": "Bienvenue dans l’espace client"}, 200
